a79a4df5b23104b41aaf785645d5a58c03983440
Clément ramondou <clementramondou@macbook-pro-de-clement.home>
2017-08-20 01:38:34 +0200

    test

--- js/function.js
+++ js/function.js
@@ -144,6 +144,7 @@ function initMap(data) {
             infowindowOpen = this.infowindow;
         });
     }
+    12
     map.addListener('rightclick', function(event) {
         newMarker = new google.maps.Marker({
             map: map,
